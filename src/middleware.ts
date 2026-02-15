a96c76814bd52ce77b609887e2baf7ed9f6d5404
NadiaMegahed18 <nadiamegahed93@gmail.com>
2026-02-16 00:24:37 +0200

    fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -16,7 +16,7 @@ export default async function middleware(req: NextRequest) {
 
   }
 
-  return NextResponse.redirect("http://localhost:3000/login")
+  return NextResponse.redirect("/login")
 
 }
 
